829af2c11979d8dd9db6a3d9172141f9609fe913
Joshua Luke <115410856+joshualukejewson@users.noreply.github.com>
2025-01-05 23:15:22 +1000

    initial commenting

---
+++ shiny_tracker.py
@@ -0,0 +1,3 @@
+
+# This is going to be a PokeDex/Shiny tracker I can use during my eventual shiny hunts across all pokemon games.
+# The app will have a search bar for the user to search for a specific pokemon, and a display area for the information about the selected pokemon.
